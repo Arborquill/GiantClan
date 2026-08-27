d878108f75a829c1b957f91be3636fe5c992f975
Arborquill <owenfn99@proton.me>
2026-08-27 16:52:55 -0700

    Update test_notion.py

--- test_notion.py
+++ test_notion.py
@@ -2,50 +2,12 @@ import os
 from notion_client import Client
 
 NOTION_TOKEN = os.environ["NOTION_TOKEN"]
-
 HISTORICAL_EVENTS_DATA_SOURCE_ID = "3b79cd66-e972-8014-9954-000b6da417a8"
+
+print("Connecting to Notion...")
 
 notion = Client(auth=NOTION_TOKEN)
 
-def get_title(properties):
-prop = properties.get("Name", {})
-title = prop.get("title", [])
-
-```
-if not title:
-    return "(untitled)"
-
-return "".join(
-    item.get("plain_text", "")
-    for item in title
-)
-```
-
-def get_relation_names(properties, property_name):
-prop = properties.get(property_name, {})
-relation = prop.get("relation", [])
-
-```
-names = []
-
-for item in relation:
-    page_id = item.get("id")
-
-    if not page_id:
-        continue
-
-    page = notion.request(
-        f"pages/{page_id}",
-        "GET"
-    )
-
-    name = get_title(page.get("properties", {}))
-    names.append(name)
-
-return names
-```
-
-print("Connecting to Notion...")
 print("Connection successful.")
 print()
 
@@ -53,8 +15,7 @@ print("=" * 70)
 print("HISTORICAL EVENTS RELATIONSHIP TEST")
 print("=" * 70)
 print()
-print("This test only reads information from Notion.")
-print("No pages or properties will be modified.")
+print("READ ONLY - NOTHING WILL BE CHANGED IN NOTION")
 print()
 
 response = notion.request(
@@ -72,29 +33,81 @@ for number, event in enumerate(events, start=1):
 properties = event.get("properties", {})
 
 ```
-event_name = get_title(properties)
-
-subjects = get_relation_names(
-    properties,
-    "Subject Cat"
-)
-
-related = get_relation_names(
-    properties,
-    "Related Cats"
-)
-
 print("-" * 70)
 print(f"EVENT {number}")
+
+name_property = properties.get("Name", {})
+title_items = name_property.get("title", [])
+
+event_name = ""
+
+for item in title_items:
+    event_name += item.get("plain_text", "")
+
+if not event_name:
+    event_name = "(untitled)"
+
 print(f"Name: {event_name}")
 
-if subjects:
-    print("Subject Cat: " + ", ".join(subjects))
+subject_property = properties.get("Subject Cat", {})
+subject_relations = subject_property.get("relation", [])
+
+subject_names = []
+
+for relation in subject_relations:
+    page_id = relation.get("id")
+
+    if page_id:
+        page = notion.request(
+            f"pages/{page_id}",
+            "GET"
+        )
+
+        page_properties = page.get("properties", {})
+        page_name_property = page_properties.get("Name", {})
+        page_title_items = page_name_property.get("title", [])
+
+        cat_name = ""
+
+        for item in page_title_items:
+            cat_name += item.get("plain_text", "")
+
+        if cat_name:
+            subject_names.append(cat_name)
+
+if subject_names:
+    print("Subject Cat: " + ", ".join(subject_names))
 else:
     print("Subject Cat: (none)")
 
-if related:
-    print("Related Cats: " + ", ".join(related))
+related_property = properties.get("Related Cats", {})
+related_relations = related_property.get("relation", [])
+
+related_names = []
+
+for relation in related_relations:
+    page_id = relation.get("id")
+
+    if page_id:
+        page = notion.request(
+            f"pages/{page_id}",
+            "GET"
+        )
+
+        page_properties = page.get("properties", {})
+        page_name_property = page_properties.get("Name", {})
+        page_title_items = page_name_property.get("title", [])
+
+        cat_name = ""
+
+        for item in page_title_items:
+            cat_name += item.get("plain_text", "")
+
+        if cat_name:
+            related_names.append(cat_name)
+
+if related_names:
+    print("Related Cats: " + ", ".join(related_names))
 else:
     print("Related Cats: (none)")
 ```
