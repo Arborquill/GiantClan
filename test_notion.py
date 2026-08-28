e269f791e94d27eac55a4e1f6f0404d90ecb537d
Arborquill <owenfn99@proton.me>
2026-08-28 08:10:47 -0700

    Update test_notion.py

--- test_notion.py
+++ test_notion.py
@@ -8,57 +8,94 @@ EVENTS_DATA_SOURCE_ID = "3b79cd66-e972-8014-9954-000b6da417a8"
 
 notion = Client(auth=NOTION_TOKEN)
 
+EXPECTED_PROPERTIES = [
+    "Kit Cats",
+    "Parent Cats",
+    "Sibling Cats",
+    "Cohort Cats",
+    "Mate Cats",
+    "Mentor Cats",
+    "Apprentice Cats",
+]
+
 print("=" * 70)
-print("EVENT DATABASE DATA SOURCE TEST")
+print("EVENT RELATION PROPERTY SCHEMA TEST")
 print("=" * 70)
 print("READ ONLY - NOTHING WILL BE CHANGED")
 print()
 
-print("Database ID:")
+print("Event Database ID:")
 print(EVENTS_DATABASE_ID)
 
 print()
-print("Retrieving database...")
-
-database = notion.databases.retrieve(
-    database_id=EVENTS_DATABASE_ID
-)
-
-print("Database retrieved successfully.")
+print("Event Data Source ID:")
+print(EVENTS_DATA_SOURCE_ID)
 
 print()
-print("=" * 70)
-print("DATA SOURCE SCHEMA")
-print("=" * 70)
+print("Retrieving data source...")
 
 data_source = notion.data_sources.retrieve(
     data_source_id=EVENTS_DATA_SOURCE_ID
 )
 
-print("Data source ID:")
-print(EVENTS_DATA_SOURCE_ID)
-
-print()
-print("Data source name:")
-print(data_source.get("name"))
-
-print()
-print("Properties returned by Notion API:")
+print("Data source retrieved successfully.")
 
 properties = data_source.get("properties", {})
 
-for property_name, property_data in properties.items():
+print()
+print("=" * 70)
+print("RELATION PROPERTY CHECK")
+print("=" * 70)
+
+all_passed = True
+
+for property_name in EXPECTED_PROPERTIES:
     print()
     print(property_name)
+
+    property_data = properties.get(property_name)
+
+    if not property_data:
+        print("ERROR: Property does not exist.")
+        all_passed = False
+        continue
+
+    property_type = property_data.get("type")
+
     print("Property type:")
-    print(property_data.get("type"))
-    print("Property ID:")
-    print(property_data.get("id"))
+    print(property_type)
+
+    if property_type != "relation":
+        print("ERROR: Property is not a relation.")
+        all_passed = False
+        continue
+
+    relation_data = property_data.get("relation", {})
+
+    print("Relation configuration:")
+    print(relation_data)
+
+    target = relation_data.get("database_id")
+
+    if target:
+        print("Target database ID:")
+        print(target)
+    else:
+        print("Target database ID:")
+        print("[NOT PROVIDED BY API]")
+
+    print("PASS: Property is a relation.")
 
 print()
 print("=" * 70)
-print("END SCHEMA")
+print("FINAL RESULT")
 print("=" * 70)
+
+if all_passed:
+    print("ALL SEVEN EVENT PROPERTIES ARE RELATIONS.")
+else:
+    print("ONE OR MORE EVENT PROPERTIES FAILED THE RELATION CHECK.")
 
 print()
 print("READ ONLY - NO DATABASES, PAGES, OR PROPERTIES WERE MODIFIED.")
+print("=" * 70)
