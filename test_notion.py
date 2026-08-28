c0bdfd8139fc5757c64fb8f49aa150854a40f41c
Arborquill <owenfn99@proton.me>
2026-08-27 20:15:53 -0700

    Update test_notion.py

--- test_notion.py
+++ test_notion.py
@@ -9,7 +9,7 @@ print("Connecting to Notion...")
 print("Connection successful.")
 print()
 print("=" * 70)
-print("HISTORICAL EVENTS RELATIONSHIP TEST")
+print("RELATION ID TO CAT NAME TEST")
 print("=" * 70)
 print()
 print("READ ONLY - NOTHING WILL BE CHANGED")
@@ -22,31 +22,82 @@ f"data_sources/{EVENTS}/query",
 )
 
 events = result.get("results", [])
-
-print("Events returned:", len(events))
-print()
-
 event = events[0]
 properties = event.get("properties", {})
 
-print("FIRST EVENT")
-print("-" * 70)
-print("Event ID:", event.get("id"))
+subject_property = properties.get("Subject Cat", {})
+subject_relations = subject_property.get("relation", [])
+
+related_property = properties.get("Related Cats", {})
+related_relations = related_property.get("relation", [])
+
+print("Subject Cat IDs:")
+print(subject_relations)
 print()
 
-print("Property names:")
-print(list(properties.keys()))
+print("Related Cats IDs:")
+print(related_relations)
 print()
 
-print("Subject Cat:")
-print(properties.get("Subject Cat"))
+subject_id_1 = subject_relations[0]["id"]
+subject_id_2 = subject_relations[1]["id"]
+subject_id_3 = subject_relations[2]["id"]
+subject_id_4 = subject_relations[3]["id"]
+
+related_id_1 = related_relations[0]["id"]
+
+cat_1 = notion.request(
+f"pages/{subject_id_1}",
+"GET"
+)
+
+cat_2 = notion.request(
+f"pages/{subject_id_2}",
+"GET"
+)
+
+cat_3 = notion.request(
+f"pages/{subject_id_3}",
+"GET"
+)
+
+cat_4 = notion.request(
+f"pages/{subject_id_4}",
+"GET"
+)
+
+cat_5 = notion.request(
+f"pages/{related_id_1}",
+"GET"
+)
+
+name_1 = cat_1.get("properties", {}).get("Name", {}).get("title", [])
+name_2 = cat_2.get("properties", {}).get("Name", {}).get("title", [])
+name_3 = cat_3.get("properties", {}).get("Name", {}).get("title", [])
+name_4 = cat_4.get("properties", {}).get("Name", {}).get("title", [])
+name_5 = cat_5.get("properties", {}).get("Name", {}).get("title", [])
+
+print("CAT 1 NAME DATA:")
+print(name_1)
 print()
 
-print("Related Cats:")
-print(properties.get("Related Cats"))
+print("CAT 2 NAME DATA:")
+print(name_2)
+print()
+
+print("CAT 3 NAME DATA:")
+print(name_3)
+print()
+
+print("CAT 4 NAME DATA:")
+print(name_4)
+print()
+
+print("CAT 5 NAME DATA:")
+print(name_5)
 print()
 
 print("=" * 70)
-print("RELATIONSHIP TEST COMPLETE")
+print("TEST COMPLETE")
 print("No Notion pages or properties were modified.")
 print("=" * 70)
